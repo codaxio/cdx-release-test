46f9c0808bdbfdfb371ffabe6c08b1ca25d5419f
Matthieu Guerra <m.guerra@ra2.tech>
2024-12-04 11:51:40 +0100

    feat: refactor

--- commands/release.ts
+++ commands/release.ts
@@ -174,7 +174,7 @@ export class Manifest {
       const files = commit.files.filter((file) => this.config.scan.some((p) => file.startsWith(path.resolve(p))))
       if (!files.length && this.config.rootPackage) root.push(commit)
       else if (files.length) {
-        let dirname = path.resolve('.')
+        let dirname = path.resolve('./')
         console.log({
           files,
           dirname,
